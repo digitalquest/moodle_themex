b7729ddd56ff26691b1aedcaf642574d7c0f4151
Sam Moulem <s.moulem@warwick.ac.uk>
2015-02-19 17:22:01 +0000

    test 17:21 breadcrumbs

--- renderers/core_renderer.php
+++ renderers/core_renderer.php
@@ -35,10 +35,31 @@ class theme_warwickclean_core_renderer extends core_renderer {
     public function navbar() {
         $items = $this->page->navbar->get_items();
         $breadcrumbs = array();
-        foreach ($items as $item) {
+/*        foreach ($items as $item) {
             $item->hideicon = true;
             $breadcrumbs[] = $this->render($item);
         }
+*/
+//Only output some nodes
+        $last_node_found = false;
+        $items = array_reverse($items);
+
+        foreach ($items as $item) {
+        //Only output crumbs with links
+			if (navigation_node::TYPE_COURSE == $item->type ||
+				(!$last_node_found && $item->action)) {
+				$last_node_found = true;
+				$item->hideicon = true;
+				$breadcrumbs[] = $this->render($item);
+			}
+
+			if (navigation_node::TYPE_COURSE == $item->type) {
+				//We're done here
+				break;
+			}
+        }
+        $breadcrumbs = array_reverse($breadcrumbs);
+		
         $divider = '<span class="divider">'.get_separator().'</span>';
         $list_items = '<li>'.join(" $divider</li><li>", $breadcrumbs).'</li>';
         $title = '<span class="accesshide">'.get_string('pagepath').'</span>';
@@ -144,6 +165,7 @@ class theme_warwickclean_core_renderer extends core_renderer {
 			$branch->add('Services',new moodle_url('/course/index.php?categoryid=56'));
 			$branch->add('Sandbox',new moodle_url('/course/index.php?categoryid=51'));
 */			
+			//Builds category list into menu
 			require_once($CFG->dirroot.'/course/lib.php');
 			$categorytree = get_course_category_tree();
 			foreach ($categorytree as $category) {
