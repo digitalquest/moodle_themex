137f39048e783c5101e9bbc1c1ba8ef37dc83d67
Sam Moulem <s.moulem@warwick.ac.uk>
2015-03-19 15:33:03 +0000

    bux fix core_renderer - Thu Mar 19 15:33:03 GMTST 2015

--- renderers/core_renderer.php
+++ renderers/core_renderer.php
@@ -96,9 +96,9 @@ class theme_warwickclean_core_renderer extends core_renderer {
         /*$list_items = '<li>'.join(" $divider</li><li>", $breadcrumbs).'</li>';*/
 		$list_items = "<li>";
 		foreach ($breadcrumbs as $breadcrumb) {
-			/*$list_items .= " $divider</li><li";
-			if (breadcrumb->type == navigation_node::TYPE_COURSE) $list_items .= " class=\'$course_class\' ";
-			$list_items .= ">$this->render($breadcrumb)";*/
+			$list_items .= " $divider</li><li";
+			//if (breadcrumb->type == navigation_node::TYPE_COURSE) $list_items .= " class=\'$course_class\' ";
+			//$list_items .= ">$this->render($breadcrumb)";
 		}
 		$list_items .= '</li>';
         $title = '<span class="accesshide">'.get_string('pagepath').'</span>';
