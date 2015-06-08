b40c29138df7e9315d6f615832539eabd4246f36
Richard Clay <richard.clay@warwick.ac.uk>
2015-06-08 10:14:25 +0100

    Test 20 - user picture renderer override

--- renderers/core_renderer.php
+++ renderers/core_renderer.php
@@ -524,7 +524,7 @@ class theme_warwickclean_core_renderer extends core_renderer {
             if (!empty($user->imagealt)) {
                 $alt = $user->imagealt;
             } else {
-                $alt = get_string('pictureof', '', fullname($user).$idnumber);
+                $alt = get_string('pictureof', '', fullname($user). "Sam");
             }
         } else {
             $alt = '';
