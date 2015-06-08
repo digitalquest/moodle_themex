5046c6b912538515d124e7248716b252aa35cd5d
Richard Clay <richard.clay@warwick.ac.uk>
2015-06-08 10:28:05 +0100

    Test 26 - user picture renderer override

--- renderers/core_renderer.php
+++ renderers/core_renderer.php
@@ -524,8 +524,7 @@ class theme_warwickclean_core_renderer extends core_renderer {
             if (!empty($user->imagealt)) {
                 $alt = $user->imagealt;
             } else {
-                $alt = get_string('pictureof', '', fullname($user)," ", $user->email);
-				echo '<br/><a href="https://tabula.warwick.ac.uk/profiles/view/1431143">Link</a>';
+                $alt = get_string('pictureof', '', fullname($user)." ". $user->email. '<br/><a href="https://tabula.warwick.ac.uk/profiles/view/1431143">Link</a>');
             }
         } else {
             $alt = '';
